afbb27080a74f76b0d024d59d4ab0947903c22d1
David Szczepanik <david.szczepanik@outlook.com>
2026-02-21 22:15:08 -0600

    all chapter2 programs

---
+++ chapter2/programs/program2-22/main.cpp
@@ -0,0 +1,18 @@
+/*
+    PROGRAM: Payroll.cpp
+    Written by Herbert Dorfmann
+    This program calculates company payroll
+    Last modified: 8/20/2012
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int employeeID;     // Employee ID number
+    double payRate;     // Employees hourly pay rate
+    double hours;       // Hours employee worked this week
+
+    // Further implementation would go below.
+    return 0;
+}
